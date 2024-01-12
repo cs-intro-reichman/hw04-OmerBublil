28f11b7876d4ed7fa274deb1602f9e94f8164069
OmerBublil <omer.bublil@post.runi.ac.il>
2024-01-12 12:53:22 +0200

    Update StringOps.java

--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,70 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        int sLength =string.length();
+        String newWord = "";
+        for (int i = 0 ; i < sLength ; i++){
+            char currentChar = string.charAt(i);
+            if (currentChar == 'a' ||currentChar ==  'e' ||currentChar ==  'i' ||currentChar ==  'o' ||currentChar ==  'u'){
+                newWord += (char)(currentChar - 32);
+            }
+            else if (currentChar == 32){
+                newWord += currentChar;
+            }
+            else if (currentChar >= 'A' && currentChar <= 'Z' && (currentChar != 'A' && currentChar != 'E' && currentChar != 'I' && currentChar != 'O' && currentChar != 'U')) {
+                newWord += (char)(currentChar + 32);
+            }
+            else {
+                newWord += currentChar;
+            }
+        }
+        return newWord;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String res = "";
+
+        for (int i = 0; i < string.length() ; i++){
+            while (string.charAt(i) == ' '){
+                i++;
+                if (res != ""){
+                  if (string.charAt(i) >= 'a' && string.charAt(i) <= 'z'){
+                    res += (char)(string.charAt(i) - 32);
+                    i++;
+                  }
+                  else if (string.charAt(i) >= 'A' && string.charAt(i) <= 'Z'){
+                    res += string.charAt(i);
+                    i++;
+                  }
+                }
+            }
+            if (string.charAt(i) >= 'A' && string.charAt(i) <= 'Z'){
+                res += (char)(string.charAt(i) + 32);
+            }
+            else {
+                res += string.charAt(i);
+            }
+        }
+                return res;
     }
 
+
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        int [] arr = new int [string.length()];
+        int size = 0;
+        for (int i = 0; i < string.length() ; i++){
+            if ((char)string.charAt(i) == chr){
+                arr [size] = i;
+                size++;
+            }
+        }
+        int [] res = new int [size];
+        for (int k = 0; k < size; k++){
+            res [k] = arr [k];
+        }
+        return res;
     }
 }
